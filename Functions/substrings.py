2863ac1ee535b31e974b2200b5c904089599dba4
MartinYUU <mreyes1@appdev.yearup.org>
2024-10-28 20:56:32 -0500

    completed Exercise 1A lab 1 Challenge

--- Functions/substrings.py
+++ Functions/substrings.py
@@ -12,11 +12,31 @@
 # parse_and_display(‘Grace Flores’) 
 # parse_and_display(‘JB Oinonen’)
 
-name = 'Mochi Reyes'
-full_name = name.split(' ')
-first_name = full_name[0]
-last_name = full_name[1]
+# name = 'Mochi Reyes'
+# full_name = name.split(' ')
+# first_name = full_name[0]
+# last_name = full_name[1]
 
-print('Full Name:', name)
-print('First Name:', first_name)
-print('Last Name:', last_name)
+# print('Full Name:', name)
+# print('First Name:', first_name)
+# print('Last Name:', last_name)
+
+
+#################################################### CHALLENGE ############################################################
+
+name = 'Megan Thee Stalion'
+full_name = name.split(' ')
+
+if len(full_name) == 1:
+    print ('Name:', full_name[0])
+elif len(full_name) == 2:
+    print(' Full Name:', name +"\n " \
+          'Frist Name:', full_name[0]+ "\n", \
+          'Last Name:', full_name[1]
+          )
+elif len(full_name) == 3:
+    print(' Full Name:', name +"\n " \
+          'Frist Name:', full_name[0]+ "\n", \
+          'Middle Name:', full_name[1] + "\n", \
+          'Last Name:', full_name[2]
+          )
